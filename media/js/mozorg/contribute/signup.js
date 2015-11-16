d7c70d31abeb5a8aa6cc6b840fd594185da6cd64
schalkneethling <sneethling@mozilla.com>
2015-11-16 17:27:56 +0200

    Fix Bug 1225170, implement new signup and task pages for contribute

---
+++ media/js/mozorg/contribute/signup.js
@@ -0,0 +1,36 @@
+var $tooltipTrigger = $('.tooltip-trigger');
+var $currentItem = {};
+var $tooltip = {};
+
+$tooltipTrigger.on('mouseenter', function() {
+    $currentItem = $(this).parents('li');
+    $tooltip = $('.tooltip', $currentItem);
+
+    $tooltip.removeClass('hidden')
+            .attr('aria-hidden', false);
+});
+
+$tooltipTrigger.on('mouseleave', function() {
+    $currentItem = $(this).parents('li');
+    $tooltip = $('.tooltip', $currentItem);
+
+    $tooltip.addClass('hidden')
+            .attr('aria-hidden', true);
+});
+
+/**
+ * Sends data to GA about the interaction steps a user has taken.
+ * @param {string} interaction - The kind of interaction
+ * @param {int} variation - Whether it is a v1 or v2 task.
+ */
+function trackInteraction(interaction, variation) {
+    window.dataLayer.push({
+        event: 'get-involved-task-selection',
+        interaction: interaction,
+        variation: variation
+    });
+}
+
+$('.simple, .challenging').on('click', 'a', function() {
+    trackInteraction(this.dataset['task'], this.dataset['variant']);
+});
